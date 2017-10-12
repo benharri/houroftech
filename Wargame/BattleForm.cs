df19aea8ddd2d551cfc6df163689c4f493ab97c8
jadams <joshuaadams18@gmail.com>
2017-10-12 14:56:24 -0400

    added logic to button attack and some bugs and stuffs

--- Wargame/BattleForm.cs
+++ Wargame/BattleForm.cs
@@ -100,14 +100,44 @@ namespace Wargame
 
         private void btnAttack_Click(object sender, EventArgs e)
         {
-            //todo: take next attacker from round stack
-            //todo: make sure attacker is still alive
-            //todo: choose and alive defnder
-            //todo: process attack
+            //take next attacker from round stack
+            var attacker = gameData.RoundOrder.Pop();
+            //make sure attacker is still alive
+            if(attacker.CurrentHP < 1)
+            {
+                messages.AppendLine("Attacker is down");
+                return;
+            }
+            //choose and alive defender
+            var opposingTeam = gameData.Team1.Contains(attacker) ? gameData.Team2 : gameData.Team1;
+            var defender = opposingTeam.FirstOrDefault(d => d.CurrentHP > 0);
+            
+            if(defender == null)
+            {
+                messages.AppendLine("No standing defenders");
+                return;
+            }
+            //process attack
+            var result = engine.DoAttack(attacker, defender);
+            messages.AppendLine(result);
             //todo: check if round is done and start next round if anyone is still alive
             messages.AppendLine(engine.DoAttack(gameData.Team1.First(), gameData.Team2.First()));
             messages.AppendLine(engine.DoAttack(gameData.Team2.First(), gameData.Team1.First()));
             RefreshLog();
+
+            //todo: check if one side has one and end fight if so
+            if(!gameData.Team1.Any(t1 => t1.CurrentHP > 0) || !gameData.Team2.Any(t2 => t2.CurrentHP > 0))
+            {
+                btnAttack.Enabled = false;
+                messages.AppendLine("Game Over");
+                //todo: start caring about who won
+                //todo: don't just end completely after one fight
+            }
+            //check if round done and start next round if anyone is still alive
+            if (!gameData.RoundOrder.Any())
+            {
+                engine.StartNextRound(gameData);
+            }
         }
 
         //todo: display initiative order
