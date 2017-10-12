1c8add73bff4680e5f209137553eed97b0676efe
Ben Harris <bharris@hagerty.com>
2017-10-12 14:12:21 -0400

    Functional dice roll

--- Wargame/GameEngine.cs
+++ Wargame/GameEngine.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Wargame
 {
@@ -11,27 +8,24 @@ namespace Wargame
         private Random rng = new Random();
 
 
-        internal int RollDice(int num, int sides)
+        internal int RollDice(int num = 1, int sides = 6)
         {
-            var total = 0;
-            for (var i = 0; i < num; i++)
-            {
-                total += rng.Next(sides) + 1;
-            }
-            return total;
+            return Enumerable.Range(1, num).Aggregate(0, (acc, _) => acc += rng.Next(1, sides + 1));
         }
 
         internal string DoAttack(Character attacker, Character defender)
         {
-            var dmg = RollDice(1, 6);
-            defender.CurrentHP -= dmg;
-            return $"{attacker.Name} dealt {dmg} damage to {defender.Name}";
+            return $"{attacker.Name} dealt {defender.CurrentHP -= RollDice(1, 6)} damage to {defender.Name}";
+            //var dmg = RollDice(1, 6);
+            //defender.CurrentHP -= dmg;
+            //return $"{attacker.Name} dealt {dmg} damage to {defender.Name}";
         }
 
         internal void StartNextRound(GameData gameData)
         {
             gameData.RoundOrder.Clear();
             // todo: implement this
+
             while (gameData.RoundOrder.Count < gameData.Team1.Count + gameData.Team2.Count)
             {
                 // todo: randomly choose a character not in RoundOrder and add it to the stack.
