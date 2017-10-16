3ca0297d92c4f2db731f1e54bf5a149eacf4a52d
Ben Harris <bharris@hagerty.com>
2017-10-16 09:12:31 -0400

    Remove reference to non-existent file

--- Wargame/BattleForm.cs
+++ Wargame/BattleForm.cs
@@ -23,7 +23,7 @@ namespace Wargame
         {
             Game = (new GameFactory()).CreateNewGame();
             Engine = new GameEngine(Game);
-            InitializeVendor();
+            //InitializeVendor();
             Engine.StartNextRound();
             btnAttack.Enabled = true;
             Messages.AppendLine($"Next up:\r\n  {Game.RoundOrder.Peek().PrintStats()}");
--- Wargame/Weapon.cs
+++ Wargame/Weapon.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wargame
+﻿namespace Wargame
 {
     public class Weapon : Item
     {
         public int MinimumDamage { get; set; }
         public int MaximumDamage { get; set; }
 
-        public Weapon(int id, string name, int minimumDamage, int maximumDamage, int price, string itemtype) : base(id, name, price, itemtype)
+        public Weapon(int id, string name, int minimumDamage, int maximumDamage, int price) : base(id, name, price)
         {
             MinimumDamage = minimumDamage;
             MaximumDamage = maximumDamage;
