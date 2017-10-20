2ac623e795ed4e278fbc1da27bae34a3c4357b59
Ben Harris <mail@benharris.ch>
2017-10-20 18:50:56 -0400

    fix formatting in gamefactory

--- Wargame/Core/GameFactory.cs
+++ Wargame/Core/GameFactory.cs
@@ -8,57 +8,57 @@ namespace Wargame
     {
         public GameData CreateNewGame()
         {
-            var data = new GameData();
-
-            data.AvailableCharacters = new BindingList<Character>
+            return new GameData
             {
-                new Mage("Eredin"),
-                new Priest("Imlerith"),
-                new Soldier("Caranthir"),
-                new Thief("Ge'els"),
-                new Mage("Avallac'h"),
-                new Witcher("Geralt"),
-                new Thief("Ciri"),
-                new Soldier("Vesemir"),
-                new Witcher("Lambert"),
-                new Witcher("Eskel"),
-                new Mage("Josh"),
-                new Thief("Dave"),
-                new Mage("CGP Grey", maxHP: 5)
+                AvailableCharacters = new BindingList<Character>
                 {
-                    BaseStrength = 6,
-                    NumDmgDice = 3,
+                    new Mage("Eredin"),
+                    new Priest("Imlerith"),
+                    new Soldier("Caranthir"),
+                    new Thief("Ge'els"),
+                    new Mage("Avallac'h"),
+                    new Witcher("Geralt"),
+                    new Thief("Ciri"),
+                    new Soldier("Vesemir"),
+                    new Witcher("Lambert"),
+                    new Witcher("Eskel"),
+                    new Mage("Josh"),
+                    new Thief("Dave"),
+                    new Mage("CGP Grey", maxHP: 5)
+                    {
+                        BaseStrength = 6,
+                        NumDmgDice = 3,
+                    },
                 },
+
+
+                Vendor = new BindingList<Item>
+                {
+                    //Weapon designer
+                    new Weapon("Sword")
+                    {
+                        Price = 5,
+                        Strength = new DiceRoll(sides: 8),
+                    },
+                    new Weapon("Rolling Pin")
+                    {
+                        Price = 100,
+                        Strength = new DiceRoll(sides: 1),
+                    },
+
+                    //Armor designer
+                    new Armor("Light Armor")
+                    {
+                        Price = 3,
+                    },
+                    new Armor("Heavy Armor")
+                    {
+                        Price = 10,
+                        Defense = 10,
+                        StrengthModifer = -2,
+                    },
+                }
             };
-
-
-            data.Vendor = new BindingList<Item>
-            {
-                //Weapon designer
-                new Weapon("Sword")
-                {
-                    Price = 5,
-                    Strength = new DiceRoll(sides: 8),
-                },
-                new Weapon("Rolling Pin")
-                {
-                    Price = 100,
-                    Strength = new DiceRoll(sides: 1),
-                },
-
-                //Armor designer
-                new Armor("Light Armor")
-                {
-                    Price = 3,
-                },
-                new Armor("Heavy Armor")
-                {
-                    Price = 10,
-                    Defense = 10,
-                    StrengthModifer = -2,
-                },
-            };
-            return data;
 
         }
     }
